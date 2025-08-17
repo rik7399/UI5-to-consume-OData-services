fea0bdf33e16435bf89477c8c7ae334f1d35bd71
Rik Dutta <rdutta7399@gmail.com>
2025-08-17 18:27:54 +0000

    Consumed /Products and displayed in View1

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,24 @@
+// sap.ui.define([
+//     "sap/ui/core/mvc/Controller"
+// ], (Controller) => {
+//     "use strict";
+
+//     return Controller.extend("sap.btp.sapui5.controller.View1", {
+//         onInit() {
+//         }
+//     });
+// });
+
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/odata/v2/ODataModel"
+], function (Controller, ODataModel) {
+  "use strict";
+
+  return Controller.extend("sap.btp.sapui5.controller.View1", {
+    onInit: function () {
+      var oModel = new ODataModel("https://cors-anywhere.herokuapp.com/https://services.odata.org/V2/Northwind/Northwind.svc/Orders?$format=json");
+      this.getView().setModel(oModel, "odm1");
+    }
+  });
+});
