0d78740aad8cb7f1c17770a5f10228c85912857c
Rik Dutta <rdutta7399@gmail.com>
2025-08-28 10:30:57 +0000

    Handling deep nested model values using Table/List

--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel"
 
-], function (Controller, JSONModel) {
+], function (Controller, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("sap.btp.sapui5.controller.View2", {
@@ -10,32 +10,45 @@ sap.ui.define([
             var oRouter = this.getOwnerComponent().getRouter();
 
             // Attach an event handler to the "patternMatched" event for the "View2" route.
-            oRouter.getRoute("View2").attachPatternMatched(this._onObjectMatched, this);
+            var sPath = oRouter.getRoute("View2").attachPatternMatched(this._onObjectMatched, this);
+
         },
         _onObjectMatched: function (oEvent) {
             // Get the parameter passed from the previous view.
             var oArgs = oEvent.getParameter("arguments");
-            
+
             var sOrderId = window.decodeURIComponent(oArgs.OrderID);
-            console.log("HELLO1");
-             var oViewModel = new JSONModel({
-                OrderID: sOrderId
+            var oViewModel = new JSONModel({
+                OrderNumber: sOrderId
             });
-            console.log("HELLO");
+            console.log(oViewModel);
             this.getView().setModel(oViewModel, "viewForView2");
-            
+
+            // Get the main OData model
+            var oModel = this.getView().getModel();
+            oModel.read("/Order_Details", {
+                filters: [
+                    new sap.ui.model.Filter("OrderID", sap.ui.model.FilterOperator.EQ, sOrderId)
+                ],
+                urlParameters: {
+                    "$expand": "Order", // Expand the Order navigation property which will be handled as a deep structure in XML
+                    "$top": 1 // Get only the top 10 results
+                },
+                success: function (oData, oResponse) {
+                    // Handle the retrieved data
+
+                    // Create new model for the new data returned
+                    var aResults = oData.results;
+                    var oTable = new JSONModel({
+                        Prod: aResults
+                    });
+                    this.getView().setModel(oTable, "returnedOrder"); //for Order Details                   
+                }.bind(this),
+                error: function (oError) {
+                    // Handle the error
+                    console.error("Error reading products:", oError);
+                }
+            });
         }
-        //,
-        // function() {
-
-
-        //     var oRouter = this.getOwnerComponent().getRouter();
-        //     var oRoute = oRouter.getRoute("View2");
-
-        //     var oViewModel = new JSONModel({
-        //         OrderID: sOrderId
-        //     });
-        //     this.getView().setModel(oViewModel, "viewForView2");
-        // }
     });
 });
